60303c7e19898ab8283ffbfa0f122463d75e272c
Xinyi <zhuxinyishcn@outlook.com>
2021-09-15 23:28:35 -0700

    feat: finish lesson4 scroll text view

--- lesson1_1/app/src/main/java/edu/neu/mad_sea/xinyizhu/lesson1_1/MainActivity.java
+++ lesson1_1/app/src/main/java/edu/neu/mad_sea/xinyizhu/lesson1_1/MainActivity.java
@@ -1,5 +1,6 @@
 package edu.neu.mad_sea.xinyizhu.lesson1_1;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -7,17 +8,20 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
 public class MainActivity extends AppCompatActivity {
 
     private int mCount = 0;
     private TextView mShowCount;
+    private AppCompatButton zeroButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mShowCount = (TextView) findViewById(R.id.show_count);
+        zeroButton = (AppCompatButton) findViewById(R.id.button_zero);
         Log.d("MainActivity", "Hello World");
     }
 
@@ -28,9 +32,18 @@ public class MainActivity extends AppCompatActivity {
 
     public void countUp(View view) {
         ++mCount;
-        if (mShowCount != null)
+        view.setBackgroundColor(Color.YELLOW);
+        if (mShowCount != null) {
             mShowCount.setText(Integer.toString(mCount));
+            if (mCount > 0) zeroButton.setBackgroundColor(Color.RED);
+        }
     }
 
+    public void resetZero(View view) {
+        mCount = 0;
+        if (mShowCount != null)
+            mShowCount.setText(Integer.toString(mCount));
+        zeroButton.setBackgroundColor(Color.rgb(170, 170, 170));
+    }
 
 }
